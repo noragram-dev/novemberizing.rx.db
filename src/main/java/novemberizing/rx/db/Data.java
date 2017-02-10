05e89ab76ca637fd227954758e6f88b0a7eb212d
novemberizing <novemberizing@gmail.com>
2017-02-10 11:41:30 +0900

    daily

---
+++ src/main/java/novemberizing/rx/db/Data.java
@@ -0,0 +1,12 @@
+package novemberizing.rx.db;
+
+import novemberizing.rx.Observable;
+
+/**
+ *
+ * @author novemberizing, me@novemberizing.net
+ * @since 2017. 2. 10.
+ */
+public class Data<T> extends Observable<T> {
+
+}
---
+++ src/main/java/redis/Pool.java
@@ -0,0 +1,9 @@
+package redis;
+
+/**
+ *
+ * @author novemberizing, me@novemberizing.net
+ * @since 2017. 2. 10.
+ */
+public class Pool {
+}
